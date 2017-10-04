087703b82a690de3889ac792a030a545d0411cfc
Kevin Choi <kevinchoinj@yahoo.com>
2017-10-04 18:16:13 -0400

    fix video width height on main page

--- src/App.js
+++ src/App.js
@@ -28,9 +28,20 @@ import bgposter from './images/vegasimage.jpg'
 class App extends Component {
   render() {
     const fillimage={
-      position:"absolute",
-      width:"100%",
-      height:"100%",
+      position:"fixed",
+      			top: "50%",
+      			left: "50%",
+      			minWidth: "100%",
+      			minHeight: "100%",
+      			width: "auto",
+      			height: "auto",
+      			zIndex: "-100",
+      			msTransform: "translateX(-50%) translateY(-50%)",
+      			MozTransform: "translateX(-50%) translateY(-50%)",
+      			WebkitTransform: "translateX(-50%) translateY(-50%)",
+      			transform: "translateX(-50%) translateY(-50%)",
+      			background: "url("+bgposter+") no-repeat",
+      			backgroundSize: "cover",
     }
     return (
       <div className="App">
